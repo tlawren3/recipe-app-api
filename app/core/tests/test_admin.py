e08314d63b97c23d609a0cfee3c46a6b55105d40
Trevor Lawrence <tlawrence@beerblip.net>
2019-10-24 09:03:41 -0600

    cleanup for environment transition

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,35 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        """Create test client and new users, and log in"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@beerblip.net',
+            password='test123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@beerblip.net',
+            password='test123',
+            name='Test User'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user admin page"""
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
